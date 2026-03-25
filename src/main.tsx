1957c398b5513828f2017f431fc8ef4241badb82
PoeySK <piz115@naver.com>
2026-03-25 11:01:59 +0900

    feat: Microsoft Clarity 설치

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import Clarity from "@microsoft/clarity";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+const clarityProjectId = import.meta.env.VITE_CLARITY_PROJECT_ID;
+
+if (clarityProjectId) {
+  Clarity.init(clarityProjectId);
+}
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
